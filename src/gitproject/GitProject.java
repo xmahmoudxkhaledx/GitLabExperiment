bb0a09972bc7832d260f68aa2dcd71d1b26ed5ce
KhairyRabeh <khairy_0123456@yahoo.com>
2019-10-26 23:02:37 +0200

    khairy rabeh commit

--- src/gitproject/GitProject.java
+++ src/gitproject/GitProject.java
@@ -4,10 +4,13 @@ public class GitProject {
     
     public static void main(String[] args) {
         printName1();
+        printName2();
     }
     
     static void printName1(){
         System.out.println("محمود خالد عبدالله محمد علي");
     }
-    
+    static void printName2(){
+        System.out.println("خيري رابح");
+    }
 }
